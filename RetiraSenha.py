d7c5553b871b5437854af7964c084e8c5bdc1321
Lucas P L <lucaslaheras@hotmail.com>
2018-10-08 17:08:33 -0300

    Interface grafica de retira senha foi feita, mas ainda nao foi inserida a logica

---
+++ RetiraSenha.py
@@ -0,0 +1,25 @@
+import kivy
+kivy.require("1.9.0")
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+
+class RetiraSenhaLayout(GridLayout):
+    def novaSenha(self, label, preferencial):
+        i = int(label) + 1
+
+        if preferencial:
+            print(str(i) + " preferencial")
+        else:
+            print(str(i) + " não preferencial")
+
+        self.display.text = str(i)
+
+class HotelApp(App):
+
+    def build(self):
+        return RetiraSenhaLayout()
+
+calcApp = HotelApp()
+calcApp.run()
+
