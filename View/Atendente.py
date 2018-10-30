9d17de4f4d173c35cefdce30ddaa448f3f4ec152
Lucas Laheras <lucaslaheras@hotmail.com>
2018-10-29 22:21:58 -0300

    CheckIn GUI

--- View/Atendente.py
+++ View/Atendente.py
@@ -90,27 +90,35 @@ class CheckOut(Screen):
         print("out")
 
 
-class PessoaIn(App):
-    def __init__(self,id='', **kwargs):
-        super().__init__(**kwargs)
-        self.ids.default.text = id
-
-
 class CheckIn(Screen):
-    def addPessQuarto(self, qtdPessoas, quarto):
-        msgPopUp = CustomPopup()
-        if int(qtdPessoas) == 0:
-            msgPopUp.call_pops('Nao pode ter 0 pessoas', 'Ok')
+    def addPessQuarto(self, nome, quarto):
+        popup = CustomPopup()
+        if nome is "" or quarto is "":
+            popup.call_pops('Nao foi preenchido todos os itens', 'Ok')
             return
 
-        print(qtdPessoas + " " + quarto)
-        msgPopUp.call_pops(qtdPessoas + ' pessoas foram adicionadas ao quarto ' + quarto, 'Ok')
-        App.get_running_app().root.current = 'menu'
+        numero = 0
 
-    def qtdPessoas(self, qtdPessoas):
-        print(qtdPessoas)
-        for i in range(int(qtdPessoas)):
-            self.ids.inputNomes.add_widget(PessoaIn(id='pessoa'+str(i)))
+        try:
+            numero = int(quarto)
+        except:
+            popup.call_pops(quarto + " nao eh um numero!", 'Ok')
+            return
+
+        # verifica se o quarto existe no hotel
+        if numero < 1 or numero > 15:
+            popup.call_pops('So existe quartos de 1 a 15', 'Ok')
+            return
+
+        # busca se o nome ja foi cadastrado
+
+        # vincula o quarto a pessoa vice versa
+
+        print(nome + " " + quarto)
+        popup.call_pops(nome + ' foi adicionadas no quarto ' + quarto, 'Ok')
+        self.ids.nomeQuarto.text = ""
+
+        App.get_running_app().root.current = 'menu'
 
 class LoginLayout(Screen):
 
